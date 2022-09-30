a80567f488234c7681e1a69bb831b9aaabfbd9ef
SarahRodrigues20 <113391554+SarahRodrigues20@users.noreply.github.com>
2022-09-30 08:15:33 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,8 @@
+sexo:str ; nome:str
+
+nome=input("Digite seu nome: ")
+sexo=input("Digite seu sexo: ")
+
+while sexo !="M" and sexo !="m":
+    nome=input("Digite seu nome: ")
+    sexo=input("Digite seu sexo: ")
